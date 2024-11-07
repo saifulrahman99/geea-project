dcd0d3629f28a5cc3561248e3a631b591efa1e6d
Saiful Rahman <sr.perante@gmail.com>
2024-11-07 16:32:23 +0700

    initial commit

---
+++ src/test/java/com/enigma/geeaproject/GeeaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.geeaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
